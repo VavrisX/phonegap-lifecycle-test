fdbef0633b0c12443c89aafb07bdb07cbf25ed59
VavrisX <vavl1_16@uni.worc.ac.uk>
2017-10-09 10:03:13 +0100

    innerHTML

--- www/js/index.js
+++ www/js/index.js
@@ -6,11 +6,10 @@ document.addEventListener("deviceready", onDeviceReady, false);
 		
 	
 function updateDisplay() {
-    document.getElementById("launched").text("Application launched: " + launched_count);
+    document.getElementById("launched").innerHTML = "Application launched: " + "launched_count";
 
-    document.getElementById("resumed").text("Application paused: " + paused_count);
-
-    document.getElementById("paused").text("Application resumed: " + resumed_count);
+    document.getElementById("resumed").innerHTML = "Application paused:" + "paused_count";
+    document.getElementById("paused").innerHTML = "Application resumed: " + "resumed_count";
 }
 
 
